904f02368c6226f7b8d56130d0beb26f4012ab08
Caroline Bixner <c.m.bixner@gmail.com>
2020-06-10 14:49:29 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# require 'faker'
+
+puts "1 - Cleaning up"
+Restaurant.destroy_all
+
+puts "2 -Creating your restaurants..."
+maris_piper = { name: "Maris Piper", address: "Prinsengracht 5, Amsterdam, 1089Ll", category: "italian" }
+la_perla = { name: "La Perla", address: "De Jordaan, Amsterdam, 7834LX", category: "chinese" }
+spaghetteria = { name: "Spaghetteria", address: "Herengracht 10, Amsterdam, 1251AB", category: "belgian" }
+binnenvisser = { name: "Binnenvisser", address: "Eerste Constantijn Huygestraat 124, 1078LL", category: "french" }
+cafe_george = { name: "Cafe George", address: "Da Costakade 3, Amsterdam, 1257AX", category: "french" }
+
+counter = 2
+[maris_piper, la_perla, spaghetteria, binnenvisser, cafe_george].each do |restaurant|
+  restaurant = Restaurant.create!(restaurant)
+  puts "#{counter += 1} - Created #{restaurant.name}"
+end
+puts "#{counter + 1}- Done and dusted!!"
+
